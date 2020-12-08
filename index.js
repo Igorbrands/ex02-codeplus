cbf26508808896e05596ccb4bb81c29b74873b33
igorbrands <igorcantelmo@gmail.com>
2020-12-07 22:33:06 -0300

    :sparkles: exercise done

--- index.js
+++ index.js
@@ -1,33 +1,44 @@
 const parentList = [
   {
-    name: "Marcos Maia",
-    children: "Clarice",
+    name: 'Marcos Maia',
+    children: 'Clarice',
   },
   {
-    name: "João Paulo",
-    children: "Pedro",
+    name: 'João Paulo',
+    children: 'Pedro',
   },
   {
-    name: "Fernanda",
-    children: "Rosa",
+    name: 'Fernanda',
+    children: 'Rosa',
   },
   {
-    name: "Patricia",
-    children: "Ana",
+    name: 'Patricia',
+    children: 'Ana',
   },
   {
-    name: "Lucas",
-    children: "Samira",
+    name: 'Lucas',
+    children: 'Samira',
   },
+];
 
-]
-
-function getParent(list, children){
-  return
+function getParent(list, children) {
+  list.filter((p) => {
+    if (p.children === children) {
+      console.log(p.name)
+    }
+  });
 }
 
-const parent = getParent(parentList, 'Pedro')
+const parent = getParent(parentList, 'Pedro');
 
-console.log(parent)
 
-//a função deve retornar o nome do pai de acordo com o nome de seu filho passado no segundo parâmetro 
+//a função deve retornar o nome do pai de acordo com o nome de seu filho passado no segundo parâmetro
+
+//solução com o metodo find(), porém este metodo para no primeiro elemento de objeto que encontrar,
+//tornando a função não viável para casos de pais com mesmo nome de filho
+// getParent = (list, children) =>
+//   list.find((p) => p.children === children).name;
+
+// const parent = getParent(parentList, 'Pedro');
+
+// console.log(parent);
